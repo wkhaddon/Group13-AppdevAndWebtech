4b6ab9474c0106092722b53f504538245624397f
Pixl <pixelbrosg@gmail.com>
2025-03-18 05:13:34 +0100

    Added rest controllers

--- backend/LearniverseConnect/src/main/java/edu/ntnu/iir/learniverse/repository/CourseProviderRepository.java
+++ backend/LearniverseConnect/src/main/java/edu/ntnu/iir/learniverse/repository/CourseProviderRepository.java
@@ -4,6 +4,10 @@ import edu.ntnu.iir.learniverse.entity.CourseProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseProviderRepository extends JpaRepository<CourseProvider, Long> {
+  List<CourseProvider> findByCourseId(Long courseId);
+  List<CourseProvider> findByProviderId(Long providerId);
 }
