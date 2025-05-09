3adfc6657f9005616629c0ace64e3c0e4d46b7bf
Elias <>
2025-05-09 13:55:02 +0200

    Switched BrowserRouter to HashRouter

--- frontend/learniverse-connect/src/main.jsx
+++ frontend/learniverse-connect/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 import './styles/main.scss';
@@ -10,9 +10,9 @@ import { AuthProvider } from '@/context/AuthContext';
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<AuthProvider>
-			<BrowserRouter>
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</AuthProvider>
 	</React.StrictMode>
 );
