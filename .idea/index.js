47ff7cebeee5df500c8f1ce8ec0e7761079f6b21
Pixl <pixelbrosg@gmail.com>
2025-05-14 02:34:07 +0200

    Updated theme and scss variables
    - improved hamburger menu a bit

--- .idea/index.js
+++
@@ -1,7 +0,0 @@
-function setSection(section) {
-    let sections = document.getElementsByTagName('section');
-    for (let i = 0; i < sections.length; i++) {
-        sections[i].style.display = 'none';
-    }
-    document.getElementById(section).style.display = 'block';
-}
